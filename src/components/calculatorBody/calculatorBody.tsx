47de4e226bd384d03b60efb6cefd0d2c7a7c2d8d
Andylarchin <stillascold@gmail.com>
2023-01-10 21:55:25 +0200

    Finished functionality product

--- src/components/calculatorBody/calculatorBody.tsx
+++ src/components/calculatorBody/calculatorBody.tsx
@@ -13,29 +13,146 @@ import './style.css';
   const reducer = (state, { type, payload }) => {
     switch (type) {
       case ACTIONS.ADD_DIGIT:
+        if (state.overwrite) {
+          return {
+            ...state,
+            currentOperand: payload.digit,
+            overwrite: false,
+          };
+        }
+        if (payload.digit === '0' && state.currentOperand === '0') {
+          return state;
+        }
+        if (payload.digit === '.' && state.currentOperand.includes('.')) {
+          return state;
+        }
+
         return {
           ...state,
-          currentopperand: `${state.currentopperand || ""}${payload.digit}`,
+          currentOperand: `${state.currentOperand || ''}${payload.digit}`,
+        };
+      case ACTIONS.CHOOSE_OPERATION:
+        if (state.currentOperand == null && state.previousOperand == null) {
+          return state;
+        }
+
+        if (state.currentOperand == null) {
+          return {
+            ...state,
+            operation: payload.operation,
+          };
+        }
+
+        if (state.previousOperand == null) {
+          return {
+            ...state,
+            operation: payload.operation,
+            previousOperand: state.currentOperand,
+            currentOperand: null,
+          };
+        }
+
+        return {
+          ...state,
+          previousOperand: evaluate(state),
+          operation: payload.operation,
+          currentOperand: null,
+        };
+      case ACTIONS.CLEAR:
+        return {};
+      case ACTIONS.DELETE_DIGIT:
+        if (state.overwrite) {
+          return {
+            ...state,
+            overwrite: false,
+            currentOperand: null,
+          };
+        }
+        if (state.currentOperand == null) return state;
+        if (state.currentOperand.length === 1) {
+          return { ...state, currentOperand: null };
+        }
+
+        return {
+          ...state,
+          currentOperand: state.currentOperand.slice(0, -1),
+        };
+      case ACTIONS.EVALUATE:
+        if (
+          state.operation == null ||
+          state.currentOperand == null ||
+          state.previousOperand == null
+        ) {
+          return state;
+        }
+
+        return {
+          ...state,
+          overwrite: true,
+          previousOperand: null,
+          operation: null,
+          currentOperand: evaluate(state),
         };
     }
-  };
+  }
+
+  const INTEGER_FORMATTER = new Intl.NumberFormat('en-us', {
+    maximumFractionDigits: 0,
+  });
+  function formatOperand(operand) {
+    if (operand == null) return;
+    const [integer, decimal] = operand.split('.');
+    if (decimal == null) return INTEGER_FORMATTER.format(integer);
+    return `${INTEGER_FORMATTER.format(integer)}.${decimal}`;
+  }
+
+  function evaluate({ currentOperand, previousOperand, operation }) {
+    const prev = parseFloat(previousOperand);
+    const current = parseFloat(currentOperand);
+    if (isNaN(prev) || isNaN(current)) return '';
+    let computation = '';
+    switch (operation) {
+      case '+':
+        computation = prev + current;
+        break;
+      case '-':
+        computation = prev - current;
+        break;
+      case '*':
+        computation = prev * current;
+        break;
+      case '÷':
+        computation = prev / current;
+        break;
+    }
+
+    return computation.toString();
+  }
 
 const CalculatorBody = () => {
 
-      const [{ currentopperand, previousopperand, operation }, dispatch] = useReducer(reducer, {});
+      const [{ currentOperand, previousOperand, operation }, dispatch] = useReducer(reducer, {});
 
 
   return (
     <div className="container">
       <div className="heading">
-        <div className="currentopperand">
-          {previousopperand} {operation}
+        <div className="previous-operand">
+          {formatOperand(previousOperand)} {operation}
         </div>
-        <div className="previousopperand">{currentopperand}</div>
+        <div className="current-operand">{formatOperand(currentOperand)}</div>
       </div>
-      <button className="space-two">AC</button>
-      <button>DEL</button>
-      <button>/</button>
+      <button className="space-two" onClick={() => dispatch({ type: ACTIONS.CLEAR, payload: '' })}>
+        AC
+      </button>
+      <button onClick={() => dispatch({ type: ACTIONS.DELETE_DIGIT })}>DEL</button>
+      <button
+        onClick={() => {
+          dispatch({ type: ACTIONS.CHOOSE_OPERATION, payload: { digit: '/' } });
+        }}
+      >
+        /
+      </button>
       <button
         onClick={() => {
           dispatch({ type: ACTIONS.ADD_DIGIT, payload: { digit: 1 } });
@@ -57,7 +174,13 @@ const CalculatorBody = () => {
       >
         3
       </button>
-      <button>*</button>
+      <button
+        onClick={() => {
+          dispatch({ type: ACTIONS.CHOOSE_OPERATION, payload: { operation: '*' } });
+        }}
+      >
+        *
+      </button>
       <button
         onClick={() => {
           dispatch({ type: ACTIONS.ADD_DIGIT, payload: { digit: 4 } });
@@ -81,7 +204,7 @@ const CalculatorBody = () => {
       </button>
       <button
         onClick={() => {
-          dispatch({ type: ACTIONS.ADD_DIGIT, payload: { digit: '+' } });
+          dispatch({ type: ACTIONS.CHOOSE_OPERATION, payload: { operation: '+' } });
         }}
       >
         +
@@ -107,8 +230,20 @@ const CalculatorBody = () => {
       >
         9
       </button>
-      <button>-</button>
-      <button>.</button>
+      <button
+        onClick={() => {
+          dispatch({ type: ACTIONS.CHOOSE_OPERATION, payload: { operation: '-' } });
+        }}
+      >
+        -
+      </button>
+      <button
+        onClick={() => {
+          dispatch({ type: ACTIONS.ADD_DIGIT, payload: { digit: '.' } });
+        }}
+      >
+        .
+      </button>
       <button
         onClick={() => {
           dispatch({ type: ACTIONS.ADD_DIGIT, payload: { digit: 0 } });
@@ -116,7 +251,9 @@ const CalculatorBody = () => {
       >
         0
       </button>
-      <button className="space-two">=</button>
+      <button className="space-two" onClick={() => dispatch({ type: ACTIONS.EVALUATE })}>
+        =
+      </button>
     </div>
   );
 };
