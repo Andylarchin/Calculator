8b272ad960be477abb09ac5a054a54c8b9852fe3
Andylarchin <stillascold@gmail.com>
2023-01-10 19:55:00 +0200

    Added calc input functionality

--- src/components/calculatorBody/calculatorBody.tsx
+++ src/components/calculatorBody/calculatorBody.tsx
@@ -1,33 +1,124 @@
-import React from 'react';
-import './style.css'
+import React, { useReducer } from 'react';
+import './style.css';
 
-const calculatorBody = () => {
+  const ACTIONS = {
+    ADD_DIGIT: 'add_digit',
+    DELETE_DIGIT: 'delete_digit',
+    CLEAR: 'clear',
+    EQUAL: 'equal',
+    CHOOSE_OPERATION: 'choose_operation',
+  };
+
+
+  const reducer = (state, { type, payload }) => {
+    switch (type) {
+      case ACTIONS.ADD_DIGIT:
+        return {
+          ...state,
+          currentopperand: `${state.currentopperand || ""}${payload.digit}`,
+        };
+    }
+  };
+
+const CalculatorBody = () => {
+
+      const [{ currentopperand, previousopperand, operation }, dispatch] = useReducer(reducer, {});
+
+
   return (
     <div className="container">
       <div className="heading">
-        <div className="currentopperand">123423</div>
-        <div className="previousopperand">123456</div>
+        <div className="currentopperand">
+          {previousopperand} {operation}
+        </div>
+        <div className="previousopperand">{currentopperand}</div>
       </div>
       <button className="space-two">AC</button>
       <button>DEL</button>
       <button>/</button>
-      <button>1</button>
-      <button>2</button>
-      <button>3</button>
+      <button
+        onClick={() => {
+          dispatch({ type: ACTIONS.ADD_DIGIT, payload: { digit: 1 } });
+        }}
+      >
+        1
+      </button>
+      <button
+        onClick={() => {
+          dispatch({ type: ACTIONS.ADD_DIGIT, payload: { digit: 2 } });
+        }}
+      >
+        2
+      </button>
+      <button
+        onClick={() => {
+          dispatch({ type: ACTIONS.ADD_DIGIT, payload: { digit: 3 } });
+        }}
+      >
+        3
+      </button>
       <button>*</button>
-      <button>4</button>
-      <button>5</button>
-      <button>6</button>
-      <button>+</button>
-      <button>7</button>
-      <button>8</button>
-      <button>9</button>
+      <button
+        onClick={() => {
+          dispatch({ type: ACTIONS.ADD_DIGIT, payload: { digit: 4 } });
+        }}
+      >
+        4
+      </button>
+      <button
+        onClick={() => {
+          dispatch({ type: ACTIONS.ADD_DIGIT, payload: { digit: 5 } });
+        }}
+      >
+        5
+      </button>
+      <button
+        onClick={() => {
+          dispatch({ type: ACTIONS.ADD_DIGIT, payload: { digit: 6 } });
+        }}
+      >
+        6
+      </button>
+      <button
+        onClick={() => {
+          dispatch({ type: ACTIONS.ADD_DIGIT, payload: { digit: '+' } });
+        }}
+      >
+        +
+      </button>
+      <button
+        onClick={() => {
+          dispatch({ type: ACTIONS.ADD_DIGIT, payload: { digit: 7 } });
+        }}
+      >
+        7
+      </button>
+      <button
+        onClick={() => {
+          dispatch({ type: ACTIONS.ADD_DIGIT, payload: { digit: 8 } });
+        }}
+      >
+        8
+      </button>
+      <button
+        onClick={() => {
+          dispatch({ type: ACTIONS.ADD_DIGIT, payload: { digit: 9 } });
+        }}
+      >
+        9
+      </button>
       <button>-</button>
       <button>.</button>
-      <button>0</button>
+      <button
+        onClick={() => {
+          dispatch({ type: ACTIONS.ADD_DIGIT, payload: { digit: 0 } });
+        }}
+      >
+        0
+      </button>
       <button className="space-two">=</button>
     </div>
   );
 };
 
-export default calculatorBody;
+export default CalculatorBody;
